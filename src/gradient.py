fd3e97f35f75f2f2cc98e182a9855e7384856141
apalvai <anoopreddy06@gmail.com>
2017-04-01 13:59:16 -0700

    Added function to determine maginitude of gradient (using both x & y gradients)

--- src/gradient.py
+++ src/gradient.py
@@ -4,8 +4,11 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 # Parameters for adjusting the min and max thresholds for x/y gradients
-GRAD_THRESH_MIN = 20
-GRAD_THRESH_MAX = 100
+GRAD_ABS_THRESH_MIN = 20
+GRAD_ABS_THRESH_MAX = 100
+
+GRAD_MAG_THRESH_MIN = 30
+GRAD_MAG_THRESH_MAX = 100
 
 def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     
@@ -30,11 +33,32 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     
     return binary_output
 
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+    # Take the gradient in x and y separately
+    sobel_gradient_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    sobel_gradient_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    
+    # Calculate the magnitude
+    abs_sobel_gradient = np.sqrt(sobel_gradient_x**2 + sobel_gradient_y**2)
+    
+    # Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scaled_sobel = np.uint8(255*abs_sobel_gradient/np.max(abs_sobel_gradient))
+    
+    # Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(scaled_sobel)
+    binary_output[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 1
+    
+    return binary_output
+
 def test():
     
     image = mpimg.imread('../test_images/test1.jpg')
-    grad_binary_x = abs_sobel_thresh(image, orient='x', thresh_min=GRAD_THRESH_MIN, thresh_max=GRAD_THRESH_MAX)
-    grad_binary_y = abs_sobel_thresh(image, orient='y', thresh_min=GRAD_THRESH_MIN, thresh_max=GRAD_THRESH_MAX)
+    grad_binary_x = abs_sobel_thresh(image, orient='x', thresh_min=GRAD_ABS_THRESH_MIN, thresh_max=GRAD_ABS_THRESH_MAX)
+    mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(GRAD_MAG_THRESH_MIN, GRAD_MAG_THRESH_MAX))
     
     # Plot the result
     f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 10))
@@ -43,8 +67,8 @@ def test():
     ax1.set_title('Original Image', fontsize=15)
     ax2.imshow(grad_binary_x, cmap='gray')
     ax2.set_title('Thresholded Gradient (X)', fontsize=15)
-    ax3.imshow(grad_binary_y, cmap='gray')
-    ax3.set_title('Thresholded Gradient (Y)', fontsize=15)
+    ax3.imshow(mag_binary, cmap='gray')
+    ax3.set_title('Gradient Maginitude', fontsize=15)
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.)
     plt.show()
 
