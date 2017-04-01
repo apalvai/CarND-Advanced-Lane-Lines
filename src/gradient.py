7a4ac303700b6d731dd6754e7687ce2907f94f64
apalvai <anoopreddy06@gmail.com>
2017-04-01 14:13:24 -0700

    Added func to dtermine direction of gradient

--- src/gradient.py
+++ src/gradient.py
@@ -10,6 +10,9 @@ GRAD_ABS_THRESH_MAX = 100
 GRAD_MAG_THRESH_MIN = 30
 GRAD_MAG_THRESH_MAX = 100
 
+GRAD_DIR_THRESH_MIN = 0.3
+GRAD_DIR_THRESH_MAX = 1.1
+
 def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     
     # Convert to grayscale (assuming the image has RGB channels)
@@ -54,11 +57,31 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     
     return binary_output
 
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+    # Take the gradient in x and y separately
+    sobel_gradient_x = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    sobel_gradient_y = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+    
+    # Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    abs_sobel_gradient = np.arctan2(sobel_gradient_y, sobel_gradient_x)
+    
+    # Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(abs_sobel_gradient)
+    binary_output[(abs_sobel_gradient >= thresh[0]) & (abs_sobel_gradient <= thresh[1])] = 1
+    
+    return binary_output
+
 def test():
     
     image = mpimg.imread('../test_images/test1.jpg')
     grad_binary_x = abs_sobel_thresh(image, orient='x', thresh_min=GRAD_ABS_THRESH_MIN, thresh_max=GRAD_ABS_THRESH_MAX)
+    grad_binary_y = abs_sobel_thresh(image, orient='y', thresh_min=GRAD_ABS_THRESH_MIN, thresh_max=GRAD_ABS_THRESH_MAX)
     mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(GRAD_MAG_THRESH_MIN, GRAD_MAG_THRESH_MAX))
+    dir_binary = dir_threshold(image, sobel_kernel=15, thresh=(GRAD_DIR_THRESH_MIN, GRAD_DIR_THRESH_MAX))
     
     # Plot the result
     f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 10))
@@ -67,8 +90,16 @@ def test():
     ax1.set_title('Original Image', fontsize=15)
     ax2.imshow(grad_binary_x, cmap='gray')
     ax2.set_title('Thresholded Gradient (X)', fontsize=15)
-    ax3.imshow(mag_binary, cmap='gray')
-    ax3.set_title('Gradient Maginitude', fontsize=15)
+    
+    # ax3.imshow(grad_binary_y, cmap='gray')
+    # ax3.set_title('Thresholded Gradient (Y)', fontsize=15)
+    
+    # ax3.imshow(mag_binary, cmap='gray')
+    # ax3.set_title('Gradient Maginitude', fontsize=15)
+    
+    ax3.imshow(dir_binary, cmap='gray')
+    ax3.set_title('Gradient Direction', fontsize=15)
+    
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.)
     plt.show()
 
