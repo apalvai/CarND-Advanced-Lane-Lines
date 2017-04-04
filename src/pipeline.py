f960537237d05fdac5bc9fef67b157c72287876d
apalvai <anoopreddy06@gmail.com>
2017-04-03 20:29:27 -0700

    Commented out intermediate plots

--- src/pipeline.py
+++ src/pipeline.py
@@ -64,17 +64,17 @@ def process_image(image):
     result = warp(thresholded_image, src_points)
     
     # Plot the result
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-    f.tight_layout()
-    
-    ax1.imshow(image)
-    ax1.set_title('Original Image', fontsize=15)
-    
-    ax2.imshow(result, cmap='gray')
-    ax2.set_title('Output', fontsize=15)
-    
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.)
-    plt.show()
+#    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+#    f.tight_layout()
+#    
+#    ax1.imshow(image)
+#    ax1.set_title('Original Image', fontsize=15)
+#    
+#    ax2.imshow(result, cmap='gray')
+#    ax2.set_title('Output', fontsize=15)
+#    
+#    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.)
+#    plt.show()
 
     return result
 
@@ -167,37 +167,37 @@ def get_line_pixels_and_fit(binary_warped, left_fit=None, right_fit=None):
     
     # Generate x and y values for plotting
     ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-    
-    # Create an image to draw on and an image to show the selection window
-    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-    window_img = np.zeros_like(out_img)
-    
-    # Color in left and right line pixels
-    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-    
-    # Generate a polygon to illustrate the search window area
-    # And recast the x and y points into usable format for cv2.fillPoly()
-    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
-    left_line_pts = np.hstack((left_line_window1, left_line_window2))
-    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
-    right_line_pts = np.hstack((right_line_window1, right_line_window2))
-    
-    # Draw the lane onto the warped blank image
-    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
-    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
-    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-    plt.imshow(result)
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
-    plt.xlim(0, 1280)
-    plt.ylim(720, 0)
-    plt.show()
-    
+#    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+#    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+#    
+#    # Create an image to draw on and an image to show the selection window
+#    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+#    window_img = np.zeros_like(out_img)
+#    
+#    # Color in left and right line pixels
+#    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+#    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+#    
+#    # Generate a polygon to illustrate the search window area
+#    # And recast the x and y points into usable format for cv2.fillPoly()
+#    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+#    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
+#    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+#    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+#    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
+#    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+#    
+#    # Draw the lane onto the warped blank image
+#    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+#    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+#    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+#    plt.imshow(result)
+#    plt.plot(left_fitx, ploty, color='yellow')
+#    plt.plot(right_fitx, ploty, color='yellow')
+#    plt.xlim(0, 1280)
+#    plt.ylim(720, 0)
+#    plt.show()
+
     return ploty, leftx, lefty, rightx, righty, left_fit, right_fit
 
 def radius_of_curvature_in_pixels(y_eval, left_fit, right_fit):
